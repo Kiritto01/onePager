72dc42a891c167a9ee9e92ae98094d56e41b40ae
Kirrito01 <patgiz204@gmail.com>
2022-01-22 21:52:24 +0100

    Poprawiony css/html, podjęta próba zmiany stylu

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
 
 //Dark/Light theme
-var r = document.querySelector(':root');
+// var r = document.querySelector(':root');
 
-function dark() {
-  r.style.setProperty('--bg-color', '#292929');
-  r.style.setProperty('--bg-color-b', '#f4f4f4');
-}
-function light() {
-  r.style.setProperty('--bg-color', '#f4f4f4');
-  r.style.setProperty('--bg-color-b', '#292929');
-}
+// function dark() {
+//   r.style.setProperty('--bg-color', '#292929');
+//   r.style.setProperty('--bg-color-b', '#f4f4f4');
+// }
+// function light() {
+//   r.style.setProperty('--bg-color', '#f4f4f4');
+//   r.style.setProperty('--bg-color-b', '#292929');
+// }
 
-let button = document.getElementById("bttn");
-function change()
-{
-    if (button.innerHTML=="DARK"){
-      button.innerHTML = "LIGHT"
-    }
-    else{
-      button.innerHTML = "DARK";
-    } 
-}
+// let button = document.getElementById("bttn");
+// function change()
+// {
+//     if (button.innerHTML=="DARK"){
+//       button.innerHTML = "LIGHT"
+//     }
+//     else{
+//       button.innerHTML = "DARK";
+//     } 
+// }
 
-button.addEventListener("click", () => {
-  change();
-  if(button.innerHTML=="DARK"){
-    light()
-  }
-  else{
-    dark()
-  }
-});
+// button.addEventListener("click", () => {
+//   change();
+//   if(button.innerHTML=="DARK"){
+//     light()
+//   }
+//   else{
+//     dark()
+//   }
+// });
 
 //scrolling
 $(document).ready(function(){
